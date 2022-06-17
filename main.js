64eaeb38af21adf28ac62a0bf67271328ee2f74f
NgocNguyen1998 <ngocnguyen09498@gmail.com>
2022-06-18 00:29:05 +0700

    Change jS file

--- main.js
+++ main.js
@@ -17,38 +17,41 @@ function tinhTienThueTNCN(id1, id2) {
     var thuNhapChiuThue = 0;
     thuNhapChiuThue = (thuNhap - 4000000 - phuThuoc * 1600000);
     var tienPhaiNop = 0;
-
-    if (thuNhapChiuThue >= 0 && thuNhapChiuThue <= 60000000) {
-        thueSuat = 0.05
-        tienPhaiNop = thuNhapChiuThue * thueSuat
-    } else if (thuNhapChiuThue > 60000000 && thuNhapChiuThue <= 120000000) {
-        thueSuat = 0.1
-        tienPhaiNop = thuNhapChiuThue * thueSuat
-    } else if (thuNhapChiuThue > 120000000 && thuNhapChiuThue <= 210000000) {
-        thueSuat = 0.15
-        tienPhaiNop = thuNhapChiuThue * thueSuat
-    } else if (thuNhapChiuThue > 210000000 && thuNhapChiuThue <= 384000000) {
-        thueSuat = 0.2
-        tienPhaiNop = thuNhapChiuThue * thueSuat
-    } else if (thuNhapChiuThue > 384000000 && thuNhapChiuThue <= 624000000) {
-        thueSuat = 0.25
-        tienPhaiNop = thuNhapChiuThue * thueSuat
-    } else if (thuNhapChiuThue > 624000000 && thuNhapChiuThue <= 960000000) {
-        thueSuat = 0.3
-        tienPhaiNop = thuNhapChiuThue * thueSuat
-    } else if (thuNhapChiuThue > 960000000) {
-        thueSuat = 0.35
-        tienPhaiNop = thuNhapChiuThue * thueSuat
-    } else {
-        tienPhaiNop = 'Không nộp thuế'
+    if (thuNhap < 0 || phuThuoc < 0 || (!Number.isInteger(phuThuoc))) {
+        tienPhaiNop = 'Không hợp lệ'
+    }
+    else {
+        if (thuNhapChiuThue > 0 && thuNhapChiuThue <= 60000000) {
+            thueSuat = 0.05
+        } else if (thuNhapChiuThue > 60000000 && thuNhapChiuThue <= 120000000) {
+            thueSuat = 0.1
+        } else if (thuNhapChiuThue > 120000000 && thuNhapChiuThue <= 210000000) {
+            thueSuat = 0.15
+        } else if (thuNhapChiuThue > 210000000 && thuNhapChiuThue <= 384000000) {
+            thueSuat = 0.2
+        } else if (thuNhapChiuThue > 384000000 && thuNhapChiuThue <= 624000000) {
+            thueSuat = 0.25
+        } else if (thuNhapChiuThue > 624000000 && thuNhapChiuThue <= 960000000) {
+            thueSuat = 0.3
+        } else if (thuNhapChiuThue > 960000000) {
+            thueSuat = 0.35
+        } else {
+            thueSuat = 0
+        }
+        
+        if (thuNhapChiuThue > 0) {
+            tienPhaiNop = (thuNhapChiuThue * thueSuat).toLocaleString();
+        } else {
+            tienPhaiNop = 'Không nộp thuế'
+        }
     }
     return tienPhaiNop;
-}
+} 
 
 document.getElementById('tinhTienThue').onclick = function () {
-    var ketQua_b1 = tinhTienThueTNCN('thuNhapNam', 'nguoiPhuThuoc').toLocaleString();
+    var ketQua_b1 = tinhTienThueTNCN('thuNhapNam', 'nguoiPhuThuoc');
     var ten = document.getElementById('nhapTen').value;
-    document.getElementById('ketQua_b1').innerHTML = 'Họ tên: ' + ten + '; Tiền thuế thu nhập cá nhân: ' + ketQua_b1 + ' VND';
+    document.getElementById('ketQua_b1').innerHTML = 'Họ tên: ' + ten + '; Tiền thuế thu nhập cá nhân phải nộp: ' + ketQua_b1 + ' VND';
 
 }
 
@@ -78,43 +81,43 @@ function tinhTienCap(id1, id2, id3) {
     var loaiKhachHang = document.getElementById(id1).value;
     var soKenh = +document.getElementById(id2).value;
     var soKetNoi = +document.getElementById(id3).value;
-    var phiHoaDon=0;
-    var phiCoBan=0;
-    var phiKenhCaoCap=0;
-    var chiPhi=0
-switch(loaiKhachHang){
-    case 'nhaDan':{
-        phiHoaDon = 4.5
-        phiCoBan = 20.5
-        phiKenhCaoCap = 7.5
-    };break
-    case 'doanhNghiep':{
-        phiHoaDon = 15
-        phiCoBan = 75
-        phiKenhCaoCap = 50
-    };break
-    default:{
-        phiHoaDon = 0
-        phiCoBan = 0
-        phiKenhCaoCap = 0
-    };
-}
-if(soKenh===0&&loaiKhachHang!==''){
-    chiPhi = phiHoaDon + phiCoBan 
-} else if(soKenh>0&&loaiKhachHang==='nhaDan'){
-    chiPhi = phiHoaDon + phiCoBan + soKenh*phiKenhCaoCap
-}else if(soKenh>0&&soKetNoi>=0&&soKetNoi<=10&&loaiKhachHang==='doanhNghiep'){
-    chiPhi = phiHoaDon + phiCoBan + phiKenhCaoCap*soKenh
-}else if(soKenh>0&&soKetNoi>10&&loaiKhachHang==='doanhNghiep'){
-    chiPhi = phiHoaDon + phiCoBan +  (soKetNoi-10)*5+ phiKenhCaoCap*soKenh
-}else if(soKenh<0||loaiKhachHang===''){
-    chiPhi = 'Không hợp lệ'
-}
-return chiPhi;
+    var phiHoaDon = 0;
+    var phiCoBan = 0;
+    var phiKenhCaoCap = 0;
+    var chiPhi = 0
+    switch (loaiKhachHang) {
+        case 'nhaDan': {
+            phiHoaDon = 4.5
+            phiCoBan = 20.5
+            phiKenhCaoCap = 7.5
+        }; break
+        case 'doanhNghiep': {
+            phiHoaDon = 15
+            phiCoBan = 75
+            phiKenhCaoCap = 50
+        }; break
+        default: {
+            phiHoaDon = 0
+            phiCoBan = 0
+            phiKenhCaoCap = 0
+        };
+    }
+    if (soKenh === 0 && loaiKhachHang !== '') {
+        chiPhi = phiHoaDon + phiCoBan
+    } else if (soKenh > 0 && loaiKhachHang === 'nhaDan') {
+        chiPhi = phiHoaDon + phiCoBan + soKenh * phiKenhCaoCap
+    } else if (soKenh > 0 && soKetNoi >= 0 && soKetNoi <= 10 && loaiKhachHang === 'doanhNghiep') {
+        chiPhi = phiHoaDon + phiCoBan + phiKenhCaoCap * soKenh
+    } else if (soKenh > 0 && soKetNoi > 10 && loaiKhachHang === 'doanhNghiep') {
+        chiPhi = phiHoaDon + phiCoBan + (soKetNoi - 10) * 5 + phiKenhCaoCap * soKenh
+    } else if (soKenh < 0 || loaiKhachHang === '') {
+        chiPhi = 'Không hợp lệ'
+    }
+    return chiPhi;
 }
 
-document.getElementById('tinhTienCap').onclick= function(){
+document.getElementById('tinhTienCap').onclick = function () {
     var maKhachHang = document.getElementById('maKhachHang').value;
-    var ketQua2 = tinhTienCap('loaiKhachHang','soKenh','soKetNoi');
-document.getElementById('ketQua_b2').innerHTML = 'Mã khách hàng: ' + maKhachHang + '; Tiền cáp: $' + ketQua2;
+    var ketQua2 = tinhTienCap('loaiKhachHang', 'soKenh', 'soKetNoi');
+    document.getElementById('ketQua_b2').innerHTML = 'Mã khách hàng: ' + maKhachHang + '; Tiền cáp: $' + ketQua2;
 }
